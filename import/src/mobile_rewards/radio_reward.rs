f1365d9af949e0d8ebecdc86dc2d402c2de3342d
Brian Balser <bbalser@nova-labs.com>
2025-06-03 13:21:30 -0400

    finish adding mobile reward data to db

--- import/src/mobile_rewards/radio_reward.rs
+++ import/src/mobile_rewards/radio_reward.rs
@@ -2,12 +2,15 @@ use chrono::{DateTime, Utc};
 use futures::stream::iter;
 use helium_proto::services::poc_mobile;
 use import_derive::Import;
+use uuid::Uuid;
 
-use crate::{PublicKeyBinary, from_proto_decimal};
+use crate::{PublicKeyBinary, determine_timestamp, from_proto_decimal};
 
 pub struct Rewards {
     radio: RadioReward,
     trust_scores: Vec<LocationTrustScore>,
+    speedtests: Vec<Speedtest>,
+    covered_hexes: Vec<CoveredHex>,
 }
 
 impl From<(DateTime<Utc>, DateTime<Utc>, poc_mobile::RadioRewardV2)> for Rewards {
@@ -23,6 +26,16 @@ impl From<(DateTime<Utc>, DateTime<Utc>, poc_mobile::RadioRewardV2)> for Rewards
                 .iter()
                 .map(|lts| LocationTrustScore::from((id.clone(), lts)))
                 .collect(),
+            speedtests: reward
+                .speedtests
+                .iter()
+                .map(|st| Speedtest::from((id.clone(), st)))
+                .collect(),
+            covered_hexes: reward
+                .covered_hexes
+                .iter()
+                .map(|ch| CoveredHex::from((id.clone(), ch)))
+                .collect(),
         }
     }
 }
@@ -31,14 +44,20 @@ impl Rewards {
     pub async fn persist(db: &db::Db, rewards: Vec<Rewards>) -> anyhow::Result<()> {
         let mut radios = Vec::new();
         let mut trust_scores = Vec::new();
+        let mut speedtests = Vec::new();
+        let mut hexes = Vec::new();
 
         for mut r in rewards {
             radios.push(r.radio);
             trust_scores.append(&mut r.trust_scores);
+            speedtests.append(&mut r.speedtests);
+            hexes.append(&mut r.covered_hexes);
         }
 
         RadioReward::persist(db, iter(radios)).await?;
         LocationTrustScore::persist(db, iter(trust_scores)).await?;
+        Speedtest::persist(db, iter(speedtests)).await?;
+        CoveredHex::persist(db, iter(hexes)).await?;
 
         Ok(())
     }
@@ -57,6 +76,31 @@ pub struct RadioReward {
     base_coverage_points_sum: f64,
     #[import(sql = "double")]
     boosted_coverage_points_sum: f64,
+    #[import(sql = "double")]
+    base_reward_shares: f64,
+    #[import(sql = "double")]
+    boosted_reward_shares: f64,
+    #[import(sql = "uint64")]
+    base_poc_reward: u64,
+    #[import(sql = "uint64")]
+    boosted_poc_reward: u64,
+    #[import(sql = "timestamptz")]
+    seniority_timestamp: DateTime<Utc>,
+    coverage_object: String,
+    #[import(sql = "double")]
+    location_trust_score_multiplier: f64,
+    #[import(sql = "double")]
+    speedtest_multiplier: f64,
+    sp_boosted_hex_status: String,
+    oracle_boosted_hex_status: String,
+    #[import(sql = "uint64")]
+    speedtest_avg_upload: u64,
+    #[import(sql = "uint64")]
+    speedtest_avg_download: u64,
+    #[import(sql = "uint32")]
+    speedtest_avg_latency_ms: u32,
+    #[import(sql = "timestamptz")]
+    speedtest_avg_timestamp: DateTime<Utc>,
 }
 
 impl From<(DateTime<Utc>, DateTime<Utc>, &poc_mobile::RadioRewardV2)> for RadioReward {
@@ -73,11 +117,108 @@ impl From<(DateTime<Utc>, DateTime<Utc>, &poc_mobile::RadioRewardV2)> for RadioR
             boosted_coverage_points_sum: from_proto_decimal(
                 reward.boosted_coverage_points_sum.as_ref(),
             ),
+            base_reward_shares: from_proto_decimal(reward.base_reward_shares.as_ref()),
+            boosted_reward_shares: from_proto_decimal(reward.boosted_reward_shares.as_ref()),
+            base_poc_reward: reward.base_poc_reward,
+            boosted_poc_reward: reward.boosted_poc_reward,
+            seniority_timestamp: determine_timestamp(reward.seniority_timestamp),
+            coverage_object: Uuid::from_slice(&reward.coverage_object)
+                .unwrap()
+                .to_string(),
+            location_trust_score_multiplier: from_proto_decimal(
+                reward.location_trust_score_multiplier.as_ref(),
+            ),
+            speedtest_multiplier: from_proto_decimal(reward.speedtest_multiplier.as_ref()),
+            sp_boosted_hex_status: reward.sp_boosted_hex_status().as_str_name().to_string(),
+            oracle_boosted_hex_status: reward.oracle_boosted_hex_status().as_str_name().to_string(),
+            speedtest_avg_upload: reward.speedtest_average.as_ref().unwrap().upload_speed_bps,
+            speedtest_avg_download: reward
+                .speedtest_average
+                .as_ref()
+                .unwrap()
+                .download_speed_bps,
+            speedtest_avg_latency_ms: reward.speedtest_average.as_ref().unwrap().latency_ms,
+            speedtest_avg_timestamp: determine_timestamp(
+                reward.speedtest_average.as_ref().unwrap().timestamp,
+            ),
         }
     }
 }
 
 #[derive(Debug, Import)]
+#[import(table_name = "mobile_reward_covered_hexes")]
+pub struct CoveredHex {
+    id: String,
+    #[import(sql = "uint64")]
+    location: u64,
+    #[import(sql = "double")]
+    base_coverage_points: f64,
+    #[import(sql = "double")]
+    boosted_coverage_points: f64,
+    urbanized: String,
+    footfall: String,
+    landtype: String,
+    #[import(sql = "double")]
+    assignment_multiplier: f64,
+    #[import(sql = "uint32")]
+    rank: u32,
+    #[import(sql = "double")]
+    rank_multiplier: f64,
+    #[import(sql = "uint32")]
+    boosted_multiplier: u32,
+    #[import(sql = "bool")]
+    service_provider_override: bool,
+}
+
+impl From<(String, &poc_mobile::radio_reward_v2::CoveredHex)> for CoveredHex {
+    fn from(value: (String, &poc_mobile::radio_reward_v2::CoveredHex)) -> Self {
+        let (id, hex) = value;
+        Self {
+            id,
+            location: hex.location,
+            base_coverage_points: from_proto_decimal(hex.base_coverage_points.as_ref()),
+            boosted_coverage_points: from_proto_decimal(hex.boosted_coverage_points.as_ref()),
+            urbanized: hex.urbanized().as_str_name().to_string(),
+            footfall: hex.footfall().as_str_name().to_string(),
+            landtype: hex.landtype().as_str_name().to_string(),
+            assignment_multiplier: from_proto_decimal(hex.assignment_multiplier.as_ref()),
+            rank: hex.rank,
+            rank_multiplier: from_proto_decimal(hex.rank_multiplier.as_ref()),
+            boosted_multiplier: hex.boosted_multiplier,
+            service_provider_override: hex.service_provider_override,
+        }
+    }
+}
+
+#[derive(Debug, Import)]
+#[import(table_name = "mobile_reward_speedtests")]
+pub struct Speedtest {
+    id: String,
+    #[import(sql = "uint64")]
+    upload: u64,
+    #[import(sql = "uint64")]
+    download: u64,
+    #[import(sql = "uint32")]
+    latency_ms: u32,
+    #[import(sql = "timestamptz")]
+    timestamp: DateTime<Utc>,
+}
+
+impl From<(String, &poc_mobile::Speedtest)> for Speedtest {
+    fn from(value: (String, &poc_mobile::Speedtest)) -> Self {
+        let (id, st) = value;
+        Self {
+            id,
+            upload: st.upload_speed_bps,
+            download: st.download_speed_bps,
+            latency_ms: st.latency_ms,
+            timestamp: determine_timestamp(st.timestamp),
+        }
+    }
+}
+
+#[derive(Debug, Import)]
+#[import(table_name = "mobile_reward_trust_scores")]
 pub struct LocationTrustScore {
     id: String,
     #[import(sql = "uint64")]
